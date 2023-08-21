fc7cd4840065054b6d6c8cbf608f1c2224f8ccdb
mr-mph <88854453+mr-mph@users.noreply.github.com>
2023-08-20 21:08:54 -0700

    only watch on dev mode

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,14 @@
 import copy from "rollup-plugin-copy-watch";
 import typescript from "rollup-plugin-typescript2";
 
+const devMode = process.env.DEV === "true";
+
 export default {
   input: "src/script.ts",
   output: {
     file: "public/bundle.js",
     format: "iife", // Immediately Invoked Function Expression
-    sourcemap: process.env.SOURCEMAP === "true",
+    sourcemap: devMode,
   },
   plugins: [
     typescript(),
@@ -15,7 +17,7 @@ export default {
         { src: "src/index.html", dest: "public" },
         { src: "src/style.css", dest: "public" },
       ],
-      watch: "src",
+      watch: devMode && "src",
     }),
   ],
 };
